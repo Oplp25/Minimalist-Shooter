739faa3db91c4014713f7de71bf0d11e122e9b5d
Oplp25 <jakobwood25@gmail.com>
2022-09-04 12:08:23 +0100

    Added Loading Functionality
    Still need to finish loading text

--- Summer Project.py
+++ Summer Project.py
@@ -6,7 +6,9 @@ pygame.init()
 win=pygame.display.set_mode((640,640))
 pygame.display.set_caption('Shooter')
 font = pygame.font.SysFont(None, 100)
+halfFont=pygame.font.SysFont(None,75)
 newFont =  pygame.font.SysFont(None, 50)
+loadTextNew=halfFont.render('Pick a save file to load',True,(255,0,0),(0,0,0))
 titleText=font.render('Shooter',True,(255,0,0),(0,0,0))
 nextLevelText=newFont.render('Next Level',True,(255,0,0),(0,200,0))
 upgradeText=newFont.render('Upgrade',True,(255,0,0),(0,200,0))
@@ -186,9 +188,10 @@ def checkMove(soldier,direction):
             if hitbox.colliderect(i.hitbox) and i!=soldier:
                 return False
         return True
+
 def upgrade():
     pass
-def save():
+def save(trueLevel):
     win.fill((0,0,0))
     text=newFont.render('Name Your Save',True,(255,0,0),(0,0,0))
     win.blit(text,(175,175))
@@ -231,13 +234,49 @@ def save():
         pygame.display.update()
         
     f=open("C:/Users/jakob/OneDrive/Documents/A-Levels/Computer Science/Misc. Code/Summer Project/Saves/"+saveName+".txt",'w')
-    f.write(str(level)+str(player.xp))
+    f.write(str(level)+','+str(player.xp))
     f.close()
-    menu()  
+    menu(trueLevel)
+
 def load():
-    saves=[name for i,j,k in os.walk('C:/Users/jakob/OneDrive/Documents/A-Levels/Computer Science/Misc. Code/Summer Project/Saves') for name in k]
-    print(saves)
-def menu():
+    saves=[('Saves/'+name) for i,j,k in os.walk('C:/Users/jakob/OneDrive/Documents/A-Levels/Computer Science/Misc. Code/Summer Project/Saves') for name in k]
+    buttons=[]
+    for i in range(len(saves)):
+        if len(saves)<=5:
+            buttons.append(pygame.Rect(80,(500/len(saves))*i+120,480,80))
+        elif len(saves)<=10:
+            if i<=4:
+                buttons.append(pygame.Rect(40,108*(i+1),240,80))
+            else:
+                buttons.append(pygame.Rect(360,108*(i-4),240,80))
+    win.fill((0,0,0))
+    win.blit(loadTextNew,(20,20))
+    for index,i in enumerate(buttons):
+        pygame.draw.rect(win,(0,200,0),i)
+        if len(saves)<=5:
+            saveInUse=saves[index].split('/')[1].split('.')[0]
+            savesText=halfFont.render(saveInUse,True,(255,0,0))
+            if len(saveInUse)<=11:
+                   win.blit(savesText,(200,(500/len(saves))*index+135))
+            else:
+                win.blit(savesText,(80,(500/len(saves))*index+135))
+    pygame.display.update()
+    loading=True
+    while loading:
+        for event in pygame.event.get():
+            if event.type==pygame.MOUSEBUTTONDOWN:
+                for index,i in enumerate(buttons):
+                    if i.collidepoint(pygame.mouse.get_pos()):
+                        f=open(saves[index],'r')
+                        line=f.readline()
+                        f.close()
+                        attributeList=line.split(',')
+                        level=attributeList[0]
+                        player.xp=attributeList[1]
+                        loading=False
+                        break
+    return level
+def menu(trueLevel):
     win.fill((0,0,0))
     for i in buttonList:
         pygame.draw.rect(win,(0,200,0),i)
@@ -254,20 +293,23 @@ def menu():
                 for i in buttonList:
                     if i.collidepoint(pygame.mouse.get_pos()):
                         if i==startButtonHitbox:
+                            beginLevel(trueLevel)
                             runMenu=False
                         elif i==upgradeButtonHitbox:
                             upgrade()
                         elif i==saveButtonHitbox:
-                            save()
+                            save(trueLevel)
                         elif i==loadButtonHitbox:
-                            load()
+                            level=load()
+                            return int(level)
             elif event.type==pygame.QUIT:
                 pygame.quit()
                 exit()
+    return True
 #initialising more variables
 player=soldier('red',300,560)
 soldiers=[player]
-level = 1
+level=1
 finalLevel=5
 
 #function that draws the GUI
@@ -281,8 +323,15 @@ def draw():
     
 #initialising more variables
 clock=pygame.time.Clock()
-menu()
-beginLevel(level)
+
+#running the menu
+menuRunning=True
+while menuRunning:
+    x1=menu(level)
+    if type(x1)==int:
+        level=x1
+    else:
+        menuRunning=False
 run=True
 won=False
 #Gameloop
@@ -318,7 +367,14 @@ while run:
             run=False
         else:
             level+=1
-            beginLevel(level)
+            #running the menu
+            menuRunning=True
+            while menuRunning:
+                x1=menu(level)
+                if type(x1)==int:
+                    level=x1
+                else:
+                    menuRunning=False
     
     #Incrementing all the variables, enemies trying to shoot and bullet moving
     for i in soldiers:
